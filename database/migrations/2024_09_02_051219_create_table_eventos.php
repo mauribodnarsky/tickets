c4592868f89fed2232196aadf9dfb160c65adee6
mauribodnarsky <mbodnarsky74@gmail.com>
2024-09-17 17:18:46 -0300

    three  commit

--- database/migrations/2024_09_02_051219_create_table_eventos.php
+++ database/migrations/2024_09_02_051219_create_table_eventos.php
@@ -21,6 +21,8 @@ class CreateTableEventos extends Migration
             $table->string('nombre');
             $table->string('descripcion')->nullable();
             $table->timeTz('fecha');
+
+            $table->timestamps();
             $table->timestamp('hora');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
       });
