38446ae21c28a3b9b29b1317a1d34de4369c873a
mauribodnarsky <mbodnarsky74@gmail.com>
2024-09-17 15:05:04 -0300

    three  commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EventoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::controller(EventoController::class)->group(function () {
+    Route::get('/generateqrcode/{id}', 'generateQRCode')->middleware(['auth'])->name('generateqrcode');
+});
 
 require __DIR__.'/auth.php';
