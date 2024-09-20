f59804c25d9d24af8a19f5cc770b86e276859c64
mauribodnarsky <mbodnarsky74@gmail.com>
2024-09-20 20:27:31 -0300

    three  commit

--- resources/views/auth/lector.blade.php
+++ resources/views/auth/lector.blade.php
@@ -2,8 +2,8 @@
     <div class="col-sm-4 shadow p-3">
       <h5 class="text-center">Escanear codigo QR</h5>
       <div class="row text-center">
-        <a id="btn-scan-qr" href="#">
-          <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/07/1499401426qr_icon.svg" class="img-fluid text-center" width="175">
+        <a id="btn-scan-qr w-100" href="#">
+          <img src="https://dab1nmslvvntp.cloudfront.net/wp-content/uploads/2017/07/1499401426qr_icon.svg" class="img-fluid text-center w-100" width="175">
         <a/>
         <canvas hidden="" id="qr-canvas" class="img-fluid"></canvas>
         </div>
@@ -13,5 +13,7 @@
       </div>
     </div>
   </div>
-  <audio id="audioScaner" src="{{asset('sonido.mp3')}}"></audio>
-  <script src="{{asset('js/index.js')}}"></script>
+
+  <audio id="audioScaner" src="{{asset('public/sonido.mp3')}}"></audio>
+    <script src="{{asset('public/qrCode.min.js')}}"></script>
+  <script src="{{asset('public/index.js')}}"></script>
